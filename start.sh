9a23649eddd0ccf81a40d7e916c258e397fb1700
Zain Raza <sraza@ptc.com>
2022-11-24 11:10:22 -0500

    Allow configurable port + README update

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 cd pandas_profiling_webapp
 
 export FLASK_APP=run.py
-export FLASK_ENV=development
-flask run
+export FLASK_DEBUG=true  # DO NOT use true in production!!
+export FLASK_PORT=${1:-5000}  # defaults to 5000
+flask run -p $FLASK_PORT
