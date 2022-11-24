86c84d6ad2c3cb24901350a90c8af33a289f6932
Zain Raza <sraza@ptc.com>
2022-11-24 11:08:38 -0500

    reformatting + allow file upload from UI

--- pandas_profiling_webapp/app/routes/load_file.py
+++ pandas_profiling_webapp/app/routes/load_file.py
@@ -2,41 +2,42 @@ from flask import render_template, session, request, redirect, url_for
 import pandas as pd
 
 from ..utils import set_session_var, check_session_var
-from ..utils import get_file_extension, retrieve_dataset_from_file
+from ..utils import get_file_extension, retrieve_dataset_from_upload
 from ..utils import generate_pandas_prof_report
 
 
 def get_load_file():
-    check_session_var('error')
+    check_session_var("error")
     return render_template("data-sources/load-file.html", session=session)
 
 
 def post_load_file():
-    data = request.form
+    data = request.files
 
-    if 'file-path' not in data:
+    if "file-upload" not in data:
         return redirect(url_for("get_load_file"))
 
-    fpath = data['file-path']
+    storage = data["file-upload"]
 
     # 1. check extension
+    fpath = storage.filename
     ext = get_file_extension(fpath)
-    valid_ext = ['csv', 'xlsx', 'xls']
+    valid_ext = ["csv", "xlsx", "xls"]
 
     if ext not in valid_ext:
-        set_session_var('error', "Le fichier n'est pas valide.")
+        set_session_var("error", "Le fichier n'est pas valide.")
         return redirect(url_for("get_load_file"))
 
-    # 2. load data
+    # 2. load the file into a pd.DataFrame
     try:
-        data = retrieve_dataset_from_file(fpath)
-    except Exception as exception:
-        set_session_var('error', "Impossible de lire le fichier.")
+        data = retrieve_dataset_from_upload(storage)
+    except Exception as _:  # most likely will be a TypeError
+        set_session_var("error", "Impossible de lire le fichier.")
         return redirect(url_for("get_load_file"))
 
     # 3. Generate pandas-profiling report
-    fpath = fpath.replace('\\','/')
-    title = fpath.split('/')[-1].split('.')[-2]
+    fpath = fpath.replace("\\", "/")
+    title = fpath.split("/")[-1].split(".")[-2]
     generate_pandas_prof_report(data, title)
 
-    return redirect(url_for("reports", name=title+'.html'))
+    return redirect(url_for("reports", name=title + ".html"))
--- pandas_profiling_webapp/app/utils/file.py
+++ pandas_profiling_webapp/app/utils/file.py
@@ -13,23 +13,32 @@ def get_file_extension(fpath):
         extension of the given file
         the text after the last dot
     """
-    return str(fpath).split('.')[-1]
+    return str(fpath).split(".")[-1]
 
 
 def retrieve_dataset_from_file(fpath, nrows=None):
-    """
-    """
+    """ """
     ext = get_file_extension(fpath)
 
-    if ext == 'csv':
-        return pd.read_csv(fpath, sep=None, engine='python', nrows=nrows)
-    elif ext in ['xls', 'xlsx']:
+    if ext == "csv":
+        return pd.read_csv(fpath, sep=None, engine="python", nrows=nrows)
+    elif ext in ["xls", "xlsx"]:
         return pd.read_excel(fpath, nrows=nrows)
     else:
         raise ValueError("L'extension n'est pas valide")
 
 
+def retrieve_dataset_from_upload(storage):
+    """ """
+    fpath = storage.filename
+    ext = get_file_extension(fpath)
+
+    if ext == "csv":
+        return pd.read_csv(storage.stream)
+    else:
+        raise ValueError("L'extension n'est pas valide")
+
+
 def check_file_is_readable(fpath):
-    """
-    """
+    """ """
     return retrieve_dataset_from_file(fpath, nrows=5)
